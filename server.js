dc1bdc6e831072c3ad8669e2f3f6dd4bd8de73f5
CLBlue60 <clblue@yahoo.com>
2024-04-29 15:47:47 -0500

    server.js file initial draft

---
+++ server.js
@@ -0,0 +1,41 @@
+// Modules
+const path = require("path");
+const express = require("express");
+const addNotes = require("./db/addNotes.js");
+const readNotes = require("./db/readNotes.js");
+const deleteNotes = require("./db/deleteNotes.js");
+
+// Server
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "./public")));
+
+// Middleware to parse JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Route to serve the homepage
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+);
+
+// Route to serve the notes page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
+);
+
+// Route to read notes from the database
+app.get("/api/notes", readNotes);
+
+// Route to add a new note to the database
+app.post("/api/notes", addNotes);
+
+// Route to delete a note from the database
+app.delete("/api/notes/:id", deleteNotes);
+
+// Start the server
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
